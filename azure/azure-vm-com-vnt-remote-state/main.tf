78508cf301ed60e9e3482b9f27a9fa02b01bdd71
Caio H. Novelletto <kionovelletto@gmail.com>
2023-08-21 20:15:25 -0300

    aulas de hoje

---
+++ azure/azure-vm-com-vnt-remote-state/main.tf
@@ -0,0 +1,32 @@
+terraform {
+
+  required_version = ">= 1.0.0"
+
+  required_providers {
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.68.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "remote"
+    storage_account_name = "storagekioremotestate"
+    container_name       = "remote-state"
+    key                  = "azure-vm/terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "terraform_remote_state" "vnet" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "remote"
+    storage_account_name = "storagekioremotestate"
+    container_name       = "remote-state"
+    key                  = "azure-vet/terraform.tfstate"
+  }
+}
---
+++ azure/azure-vm-com-vnt-remote-state/vm.tf
@@ -0,0 +1,65 @@
+resource "azurerm_resource_group" "resource_group" {
+  name     = "vm"
+  location = var.location
+  tags     = local.commom_tags # A variavel esta apontando para o arquivo locals.tf
+}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "public-ip-terraform"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = var.location
+  allocation_method   = "Dynamic"
+
+  tags = local.commom_tags
+}
+
+resource "azurerm_network_interface" "azurerm_network_interface" {
+  name                = "network-interface-terraform"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  ip_configuration {
+    name                          = "public-ip-terraform"
+    subnet_id                     = data.terraform_remote_state.vnet.outputs.subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.public_ip.id
+  }
+  tags = local.commom_tags
+}
+
+resource "azurerm_network_interface_application_security_group_association" "nisga" {
+  network_interface_id          = azurerm_network_interface.azurerm_network_interface.id
+  application_security_group_id = data.terraform_remote_state.vnet.outputs.security_group_id
+}
+
+esource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-terraform"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = var.location
+  size                = "Standard_B1s"
+  admin_username      = "terraform"
+  network_interface_ids = [azurerm_network_interface.network_interface.id]
+
+  admin_ssh_key {
+    username   = "terraform"
+    public_key = file("./azure-key.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+  tags = local.commom_tags
+}
