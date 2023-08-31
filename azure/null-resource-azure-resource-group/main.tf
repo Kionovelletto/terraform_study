048b941ac1cad7994d7f9a06bca1b907812b3f36
Caio H. Novelletto <kionovelletto@gmail.com>
2023-08-31 19:14:22 -0300

    new aula

---
+++ azure/null-resource-azure-resource-group/main.tf
@@ -0,0 +1,22 @@
+terraform {
+
+  required_version = ">= 1.0.0"
+
+  required_providers {
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.68.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "remote"
+    storage_account_name = "storagekioremotestate"
+    container_name       = "remote-state"
+    key                  = "azure-resource-group-null-resource/terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
---
+++ azure/null-resource-azure-resource-group/variables.tf
@@ -0,0 +1,5 @@
+variable "location" {
+  description = "Variavel indicador de região onde os recursos vão ser criados"
+  type        = string
+  default     = "West US 2"
+}
